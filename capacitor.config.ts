d2bae8efd5f36cb461936b97eebdf1316f851ed1
reegan01 <reeganbose250691@gmail.com>
2025-03-22 18:28:53 +0400

    Github actions init

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vuecapgithubactions.test',
+  appName: 'vue-cap-githubactions-test',
+  webDir: 'dist'
+};
+
+export default config;
